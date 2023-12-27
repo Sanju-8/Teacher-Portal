474285d4c7ce8dff79c2ce6090a262640dded39a
Sanju <sanju.santhosh@yatnam.com>
2023-12-27 14:11:51 +0530

    Add Config.example.php

--- connection.php
+++ connection.php
@@ -1,3 +1,9 @@
 <?php
     include("config.php");
+    $con = new mysqli(SERVER, USERNAME, PASSWORD, DB);
+    if($con->connect_error){
+        echo "failed";
+    }else{
+        echo "success";
+    }
 ?>
