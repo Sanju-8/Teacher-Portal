7a280074a9a0138e002202b5ea594e55725d7264
Sanju <sanju.santhosh@yatnam.com>
2023-12-27 14:22:07 +0530

    Update Done In Connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
 <?php
+    if (!file_exists('config.php')) {
+        die('Error: config.php file not found. Please create the config.php file.');
+    }else{
     include("config.php");
     $con = new mysqli(SERVER, USERNAME, PASSWORD, DB);
-    if($con->connect_error){
-        echo "failed";
-    }else{
-        echo "success";
     }
 ?>
