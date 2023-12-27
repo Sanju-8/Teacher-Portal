2398ac84ab682d1e0e749c970716f9ac46c6b456
Sanju <sanju.santhosh@yatnam.com>
2023-12-27 15:20:56 +0530

    Update connection.php.

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     if (!file_exists('config.php')) {
         die('Error: config.php file not found. Please create the config.php file.');
     }else{
-    include("config.php");
-    $con = new mysqli(SERVER, USERNAME, PASSWORD, DB);
+        include("config.php");
+        $con = new mysqli(SERVER, USERNAME, PASSWORD, DB);
     }
 ?>
